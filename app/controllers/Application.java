2a74951dc79b8eaafc9257b2cc8bf34993cb1585
2156744b <2156744b@student.gla.ac.uk>
2015-03-08 20:40:41 +0000

    Testing objects to json

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -4,7 +4,6 @@ import play.*;
 import play.mvc.*;
 import play.libs.Json;
 import views.html.*;
-
 import utils.database.*;
 
 public class Application extends Controller {
@@ -16,9 +15,21 @@ public class Application extends Controller {
 	public static Result postgis_version(){
 		
 		Queries q = new Queries();
-		String result = q.postgis_version();
+		String postgis = q.postgis_version();
+
+		Postgis_version v = new Postgis_version(postgis);
 		
-		return ok(Json.toJson(result));
+		return ok(Json.toJson(v));
+		
+	}
+	
+	private static class Postgis_version{
+		
+		public String version = "";
+		
+		public Postgis_version(String v){
+			this.version = v;
+		}
 		
 	}
 
