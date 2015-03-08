f5eafbc016b619ddf7ec2afd411f6ad391def0e9
2156744b <2156744b@student.gla.ac.uk>
2015-03-08 18:30:15 +0000

    Moved database connection

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,58 +1,28 @@
 package controllers;
 
 import play.*;
-import java.sql.DriverManager;
-import java.sql.Connection;
-import java.sql.SQLException;
 import play.mvc.*;
-
 import views.html.*;
+import database.PSQLConnection;
+import java.sql.Connection;
 
 public class Application extends Controller {
 
-    public static Result index() {
-    	
-    	String dbstatus = "";
-    	
-    	try {
-    		 
-			Class.forName("org.postgresql.Driver");
- 
-		} catch (ClassNotFoundException e) {
-			dbstatus = e.toString();
-		}
- 
- 
-		Connection connection = null;
- 
-		try {
- 
-			String host = System.getenv("OPENSHIFT_POSTGRESQL_DB_HOST");
-			String port = System.getenv("OPENSHIFT_POSTGRESQL_DB_PORT");
-			String database = "mhciback";
-			String username = "admin8n47slb";
-			String password = "ZuULP8RHLy2n";
-			
-			connection = DriverManager.getConnection(
-					"jdbc:postgresql://" + host + ":" + port + "/" + database, username,
-					password);
- 
-		} catch (SQLException e) {
- 			dbstatus = e.toString(); 
-		}
- 
-		if (connection != null) {
+	public static Result index() {
+
+		String dbstatus = "";
+
+		PSQLConnection p = new PSQLConnection();
+		Connection c = p.connect();
+
+		if (c != null) {
 			dbstatus = "Connected!";
-			try {
-				connection.close();
-			} catch (SQLException e) {
-				dbstatus = e.toString();
-			}
-		} else {
-			dbstatus = "Connection failed";
-		}
-    	
-        return ok(index.render("Hello mobile HCI experts, database status: " + dbstatus));
-    }
+			p.disconnect(c);
+		} else
+			dbstatus = "Not connected";
+
+		return ok(index.render("Hello mobile HCI experts, <br> database status: "
+				+ dbstatus));
+	}
 
 }
