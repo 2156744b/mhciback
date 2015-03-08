a0b569e186617c2962efbf033f73a8178fbf2341
2156744b <2156744b@student.gla.ac.uk>
2015-03-08 01:46:41 +0000

    Database testing

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,6 +1,9 @@
 package controllers;
 
 import play.*;
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.SQLException;
 import play.mvc.*;
 
 import views.html.*;
@@ -8,7 +11,45 @@ import views.html.*;
 public class Application extends Controller {
 
     public static Result index() {
-        return ok(index.render("Hello mobile HCI experts"));
+    	
+    	String dbstatus = "";
+    	
+    	try {
+    		 
+			Class.forName("org.postgresql.Driver");
+ 
+		} catch (ClassNotFoundException e) {
+			dbstatus = e.printStackTrace();
+		}
+ 
+ 
+		Connection connection = null;
+ 
+		try {
+ 
+			String host = System.getenv("OPENSHIFT_POSTGRESQL_DB_HOST");
+			String port = System.getenv("OPENSHIFT_POSTGRESQL_DB_PORT");
+			String database = "mhciback";
+			String username = "admin8n47slb";
+			String password = "ZuULP8RHLy2n";
+			
+			connection = DriverManager.getConnection(
+					"jdbc:postgresql://" + host + ":" + port + "/" + database, username,
+					password);
+ 
+		} catch (SQLException e) {
+ 			dbstatus = e.printStackTrace(); 
+		}
+ 
+		if (connection != null) {
+			dbstatus = "Connected!";
+			connection.close();
+		} else {
+			dbstatus = "Connection failed";
+		}
+	}
+    	
+        return ok(index.render("Hello mobile HCI experts, database status: " + dbstatus));
     }
 
 }
